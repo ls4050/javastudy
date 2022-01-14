33c9ee42348051f8d217d6ba5b0c0c29b6885377
Wonseok <ls4050@naver.com>
2022-01-14 21:16:25 +0900

    practice01 - prob5

--- practice01/src/main/java/prob5/Prob5.java
+++ practice01/src/main/java/prob5/Prob5.java
@@ -3,5 +3,24 @@ package prob5;
 public class Prob5 {
 
 	public static void main(String[] args) {
+		String str = "";
+
+		for (Integer i = 0; i < 99; i++) {
+			if (i.toString().length() == 1) {
+				if (i.toString().charAt(0) == '3' || i.toString().charAt(0) == '6' || i.toString().charAt(0) == '9') {
+					str += "짝";
+					System.out.print(i + " " + str + "\n");
+				}
+			} else {
+				if (i.toString().charAt(0) == '3' || i.toString().charAt(0) == '6' || i.toString().charAt(0) == '9') {
+					str += "짝";
+				}
+				if (i.toString().charAt(1) == '3' || i.toString().charAt(1) == '6' || i.toString().charAt(1) == '9') {
+					str += "짝";
+					System.out.print(i + " " + str + "\n");
+				}
+			}
+			str = "";
+		}
 	}
 }
