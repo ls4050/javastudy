195458165e88a76823f7873b38d4130999bdedec
Wonseok <ls4050@naver.com>
2022-01-14 09:05:28 +0900

    practice 1 - prb3

--- practice01/src/main/java/prob2/Prob2.java
+++ practice01/src/main/java/prob2/Prob2.java
@@ -2,6 +2,13 @@ package prob2;
 
 public class Prob2 {
 	public static void main(String[] args) {
-		/* 코드 작성 */
+		int count = 10;
+		for(int i=1; i<10; i++) {
+			for(int j=i; j<=count; j++) {
+				System.out.print(j+ " ");
+			}
+			System.out.println();
+			count++;
+		}
 	}
 }
--- practice01/src/main/java/prob3/Prob3.java
+++ practice01/src/main/java/prob3/Prob3.java
@@ -7,9 +7,22 @@ public class Prob3 {
 	public static void main(String[] args) {
 
 		Scanner scanner = new Scanner(System.in);
-
-		/* 코드 작성 */
 		
+		System.out.print("숫자를 입력하세요 : ");
+		int num = scanner.nextInt();
+		int result = 0;
+		int count = num/2+1;
+		
+		
+		for(int i=0;i<count;i++) {
+			if(num%2==0) {
+				result += i*2;
+			} else {
+				result += i*2+1;
+			}
+		}
+		
+		System.out.print("결과 값 : " + result);
 		scanner.close();
 	}
 }
