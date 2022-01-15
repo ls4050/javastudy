b29a5c8aa9ca149be10aa65be77ed882c7a3052c
Wonseok <ls4050@naver.com>
2022-01-15 16:14:07 +0900

    practice02 - prb03

--- practice02/src/main/java/prob03/Prob03.java
+++ practice02/src/main/java/prob03/Prob03.java
@@ -1,17 +1,38 @@
 package prob03;
 
+import java.util.Arrays;
+
 public class Prob03 {
 	public static void main(String args[]) {
 		char c[] = { 'T', 'h', 'i', 's', ' ', 'i', 's', ' ', 'a', ' ', 'p', 'e', 'n', 'c', 'i', 'l', '.' };
-
+		
+		
 		// 원래 배열 원소 출력
 		printCharArray(c);
-
+		
 		// 공백 문자 바꾸기
 		replaceSpace(c);
 
 		// 수정된 배열 원소 출력
 		printCharArray(c);
+	}
+	
+
+
+
+	private static void printCharArray(char[] c) {
+		for(char s : c) {
+			System.out.print(s);
+		}
+		System.out.println();
+	}
+	private static void replaceSpace(char[] c) {
+		
+		for(int i =0; i<c.length; i++) {
+			if(c[i]==' ') {
+				c[i] = ',';
+			}
+		}
 	}
 
 }
