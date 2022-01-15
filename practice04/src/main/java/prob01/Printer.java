cbc530e0f3c448f06fbec114bb7eebc95c27af79
Wonseok <ls4050@naver.com>
2022-01-15 19:17:52 +0900

    practice04 - prb01 solved

---
+++ practice04/src/main/java/prob01/Printer.java
@@ -0,0 +1,19 @@
+package prob01;
+
+public class Printer {
+	protected void println(boolean b) {
+		System.out.println(b);
+	}
+
+	protected void println(int i) {
+		System.out.println(i);
+	}
+
+	protected void println(double d) {
+		System.out.println(d);
+	}
+
+	protected void println(String s) {
+		System.out.println(s);
+	}
+}
