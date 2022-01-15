871ad98b2c663d18da71b94f47c5f81690e64dca
Wonseok <ls4050@naver.com>
2022-01-15 15:01:23 +0900

    practice02 - prob01

--- practice02/src/main/java/prob01/Prob01.java
+++ practice02/src/main/java/prob01/Prob01.java
@@ -9,6 +9,13 @@ public class Prob01 {
 		final int[] MONEYS = { 50000, 10000, 5000, 1000, 500, 100, 50, 10, 5, 1 };
 
 		/* 코드 작성 */
+		System.out.print("금액 : ");
+		int num = scanner.nextInt();
+		
+		for(int i=0; i<10; i++) {
+			System.out.println(MONEYS[i]+"원 : "+num/MONEYS[i]+"개");
+			num = num - MONEYS[i]*(num/MONEYS[i]);
+		}
 		
 		scanner.close();
  	}
