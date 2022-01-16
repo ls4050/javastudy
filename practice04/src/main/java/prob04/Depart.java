c7012a0c541e8326b8d376cb5aa254c96eb99ed4
Wonseok <ls4050@naver.com>
2022-01-16 23:49:37 +0900

    practice04 - prb04 solved

--- practice04/src/main/java/prob04/Depart.java
+++ practice04/src/main/java/prob04/Depart.java
@@ -1,5 +1,15 @@
 package prob04;
 
-public class Depart {
-
+public class Depart extends Employee{
+	private String department;
+	
+	Depart(String name, int salary, String department) {
+		super(name, salary);
+		this.department = department;
+	}
+	
+	@Override
+	public void getInformation() {
+		System.out.println( "이름:" + super.getName() + " 연봉:" + super.getSalary() + " 부서:" + department);
+	}
 }
