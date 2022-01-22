94d7b4aac1a9eae25fe2e0175e8645ca8772a545
Wonseok <ls4050@naver.com>
2022-01-23 02:14:18 +0900

    practice05 - prb5 solved

--- practice05/src/main/java/prob5/MyStack.java
+++ practice05/src/main/java/prob5/MyStack.java
@@ -1,4 +1,27 @@
 package prob5;
 
 public class MyStack {
+	String[] stack = null;
+	Integer top=0;
+	
+	MyStack(int size){
+		stack = new String[5];
+	}
+	
+	public void push(String str) {
+		stack[top++] = str;
+	}
+	
+	public String pop() throws MyStackException {
+		if(top<=0) 
+			throw new MyStackException("stack is empty");
+		else
+			return stack[--top];
+	}
+	
+	public boolean isEmpty() {
+		boolean result = false;
+		if(top==0) result=true;
+		return result;
+	}
 }
